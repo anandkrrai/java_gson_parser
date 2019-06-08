771a67ee8df0ed9a23f2cdc0e2d4bb3dffc1265a
anand rai <kumar.anand.rai@gmail.com>
2019-06-08 23:34:27 +0530

    model added

--- src/main/java/rai/aid/App.java
+++ src/main/java/rai/aid/App.java
@@ -10,24 +10,21 @@ public class App {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-//		System.out.println("Hello World!");
-//		Gson gson = new Gson();
-//		JsonReader reader = new JsonReader(new FileReader("file.json"));
-//		List<Recommendations> data = gson.fromJson(reader, REVIEW_TYPE); // contains the whole reviews list
-//		data.toScreen();
-//		JsonParser parser = new JsonParser();
-//		JsonElement jsonTree = parser.parse(extra.str);
-//		if (jsonTree.isJsonObject()) {
-//			JsonObject jsonObject = jsonTree.getAsJsonObject();
-//			JsonElement element = jsonObject.get("api_version");
-//			System.out.println(element);
-//		}
+		System.out.println("Hello World!");
+		Gson gson = new Gson();
+		JsonParser parser = new JsonParser();
+		JsonElement jsonTree = parser.parse(extra.str);
+		if (jsonTree.isJsonObject()) {
+			JsonObject jsonObject = jsonTree.getAsJsonObject();
+			JsonElement element = jsonObject.get("api_version");
+			System.out.println(element);
+		}
 		
-		  Gson gson = new Gson();
-
-	  BufferedReader br = new BufferedReader(new FileReader("//home//anand//eclipse-workspace//aid//src//main//java//rai//aid//file.json"));
-	   Recommendations recommendations = gson.fromJson(br, Recommendations.class);
-	   System.out.println(recommendations);
+//		Gson gson = new Gson();
+//
+//	BufferedReader br = new BufferedReader(new FileReader("//home//anand//eclipse-workspace//aid//src//main//java//rai//aid//file.json"));
+//	   Recommendations recommendations = gson.fromJson(br, Recommendations.class);
+//	   System.out.println(recommendations);
 
 	}
 }
